0bbf6524117894896f5e387df249ace40bc69334
Nurullokhon <olimovnurullo2001@gmail.com>
2023-02-15 21:26:38 +0500

    "MainScreen" - added LazyVerticalGrid

---
+++ app/src/main/java/com/example/filmsappcompose/main_screen/domain/Film.kt
@@ -0,0 +1,11 @@
+package com.example.filmsappcompose.main_screen.domain
+
+data class Film(
+    val id: Int,
+    val name: String,
+    val image: String,
+    val date_publication: Long,
+    val rating: Byte,
+    val description: String,
+    val category: Int
+)
